19746d723ca31c5affceb1169f04687445b3ac7f
AleksBasko <aleks.spiegel@gmail.com>
2017-07-10 23:41:03 +0300

    sudoku

--- Sudoku/src/js/part/all.js
+++ Sudoku/src/js/part/all.js
@@ -1,4 +1,3 @@
-
 // window.onload = function() {
 //
 //     var arr = [1,2,3,4,5,6,7,8,9];
@@ -81,63 +80,108 @@
 //
 // });
 
-$(document).ready(function() {
-    count();
+$(document).ready(function () {
 
+    select();
 });
 
+    function select() {
+        // var row = [];
+        // var count = 10;
+        //
+        // function getRandomArbitary(min, max) {
+        //     return Math.random() * (max - min) + min;
+        // }
+        //
+        // function searchRandom(){
+        //     var rdm = Math.floor(getRandomArbitary(1, 20));
+        //
+        //     for(var i=0; i < row.length; i++){
+        //         if(rdm == row[i]) {
+        //             setTimeout(function() {
+        //                 searchRandom();
+        //             }, 1);
+        //         }
+        //     }
+        //     row.push(rdm);
+        // }
+        //
+        // for(var i=0; i < count; i++){
+        //     searchRandom();
+        //     console.log(row);
+        // }
 
-function count() {
 
-    var arr=[1,2,3,4,5,6,7,8,9];
-    var row1 =[];
+        var row = [];
+        var count = 10;
 
-    function getRandomArbitary(min, max) {
-        return Math.random() * (max - min) + min;
-    }
+        function getRandomArbitary(min, max) {
+            return Math.random() * (max - min) + min;
+        }
 
-    // var rndm = Math.floor(getRandomArbitary(1,9));
+        function searchRandom() {
+            var rdm = Math.floor(getRandomArbitary(1, 20));
 
-    function block1() {
-        var newArr = arr.sort(function() {
-            return .5 - Math.random();
-        });
-
-        for (var i=0; i <= 3; i++) {
-            if(i<3) {
-                row1.push(newArr[i]);
+            if (row.includes(rdm)) {
+                searchRandom();
+            } else {
+                row.push(rdm);
             }
         }
-    }
-    block1();
 
-    function rndm(a, b) {
-        var rdm = Math.floor(getRandomArbitary(1,9));
+        for (var i = 0; i < count; i++) {
+            searchRandom();
+        }
 
-        if(a == rdm) {
-            rndm(a);
-        }
-        else {
-            console.log(b);
-            // b.push(rdm);
-        }
+        console.log(row);
+
     }
 
-    function block2() {
-
-        for (var i=0; i < row1.length; i++) {
-            rndm(row1[i], row1);
-        }
-    }
-    block2();
-    console.log(row1);
-}
-
-
-
-
-
-
+// function count() {
+//
+//     var arr=[1,2,3,4,5,6,7,8,9];
+//     var row1 =[];
+//
+//     function getRandomArbitary(min, max) {
+//         return Math.random() * (max - min) + min;
+//     }
+//
+//     // var rndm = Math.floor(getRandomArbitary(1,9));
+//
+//     function block1() {
+//         var newArr = arr.sort(function() {
+//             return .5 - Math.random();
+//         });
+//
+//         for (var i=0; i <= 3; i++) {
+//             if(i<3) {
+//                 row1.push(newArr[i]);
+//             }
+//         }
+//     }
+//     block1();
+//
+//     function rndm(a, b) {
+//         var rdm = Math.floor(getRandomArbitary(1,9));
+//
+//         if(a == rdm) {
+//             rndm(a);
+//         }
+//         else {
+//             console.log(b);
+//             // b.push(rdm);
+//         }
+//     }
+//
+//     function block2() {
+//
+//         for (var i=0; i < row1.length; i++) {
+//             rndm(row1[i], row1);
+//         }
+//     }
+//     block2();
+//     console.log(row1);
+// }
 
 
 // .sg-squer
