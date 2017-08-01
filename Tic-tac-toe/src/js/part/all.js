bf727542020de4ebe29794fb20ad7616ab63d06b
AleksBasko <aleks.spiegel@gmail.com>
2017-08-01 23:31:44 +0300

    tic tac style

--- Tic-tac-toe/src/js/part/all.js
+++ Tic-tac-toe/src/js/part/all.js
@@ -1,68 +1,75 @@
-$(document).ready(function(){
 
-    var arr = [0,1,2,3,4,5,6,7,8];
-    var arr2 = [0,1,2,3,4,5,6,7,8];
-    var tex = '+';
-    click(arr2, tex, arr);
+window.onload = function() {
 
-});
+    let arr = [0,1,2,3,4,5,6,7,8];
+    let firstP = '<svg class="tac" viewBox="0 -10 128 128" width="55px" height="55px"><path d="M16,16L112,112"></path><path d="M112,16L16,112"></path></svg>';
+    let secondP = '<svg class="tic"><circle r="21" cx="35" cy="35"></circle></svg>';
 
 
-function click(a, b, c) {
+    click(arr, firstP, secondP);
+    
+};
 
-    $('.tic-item').click(function(){
-        if (!$(this).hasClass('full')) {
 
-            var val = $(this).data( "value" );
-            console.log('нажали на ' + val);
 
-            for (var i = 0; i < a.length; i++) {
-                if(a[i] == val) {
-                    console.log('удалили ' + a[i]);
-                    a.splice(i, 1);
-                    break;
+function click(array, point1, point2) {
+    if(array.length) {
+        let ticItem = document.getElementsByClassName('tic-item');
+
+        for (let i = 0; i < ticItem.length; i++) {
+            ticItem[i].addEventListener('click', function () {
+                if (!this.classList.contains('full')) {
+                    let data = this.dataset;
+                    let val = data.value;
+
+                    for (let j = 0; j < array.length; j++) {
+                        if (array[j] == val) {
+                            array.splice(j, 1);
+                            break;
+                        }
+                    }
+                    console.log(array);
+
+                    this.classList.add('full');
+                    this.innerHTML = point1;
+                    another(array, point2, ticItem);
                 }
-            }
-
-            // var index = a.indexOf(val);
-            //
-            // if(index !==-1){
-            //     a.splice(index,1);
-            // }
-            console.log('теперь массив ' + a);
+            })
         }
-
-        $(this).addClass('full');
-        $(this).text(b);
-        another(a, c);
-    });
+    }
 }
 
 
 
-function another(freeArray, bb) {
-    var rdm = Math.floor(getRandomArbitary(0, freeArray.length));
-    console.log(freeArray);
-    // console.log(rdm);
+function another(freeArray, point2, item) {
+    if(freeArray.length) {
+        let rdm = Math.floor(getRandomArbitary(0, freeArray.length));
+        console.log(freeArray);
+        console.log('rdm ' + rdm);
 
-    var ds = freeArray[rdm];
-    console.log('ds ' + ds);
+        let selectPoint = freeArray[rdm];
+        console.log('ds ' + selectPoint);
 
-    var item = $('.tic-item');
-    // console.log(item);
+        let freeItem = item;
+        // console.log(item);
 
-    item.each(function() {
-        var val = $(this).data( "value" );
-        if(val == ds) {
-            $(this).text('0');
-            console.log('val ' + val);
-            var index = freeArray.indexOf(val);
+        for (let i=0; i < freeItem.length; i++) {
+            let data = freeItem[i].dataset;
 
-            if(index !==-1){
-                freeArray.splice(index,1);
+            let val = data.value;
+
+            // console.log('selectPoint ' + selectPoint);
+            if(val == selectPoint) {
+                console.log('val ' + val);
+                freeItem[i].classList.add('full');
+                freeItem[i].innerHTML = point2;
+
+                freeArray.splice(rdm,1);
+
+                // console.log(freeArray);
             }
         }
-    });
+    }
 
 }
 
@@ -70,86 +77,3 @@ function another(freeArray, bb) {
 function getRandomArbitary(min, max) {
     return Math.random() * (max - min) + min;
 }
-
-// $(document).ready(function() {
-//     $('.select').each(function() {
-//         if ($(this).is(':checked')) {
-//             console.log($(this).data('value'));
-//             $(this).addClass('active');
-//         }
-//     })
-//     $('.select').click(function() {
-//         if (!$(this).hasClass('active')) {
-//             console.log('Меняем ' + $(this).data('value'));
-//             $('.select').removeClass('active');
-//             $(this).addClass('active');
-//         }
-//     })
-// });
-
-$(document).ready(function() {
-    // var canvas = document.createElement('canvas');
-    //
-    // canvas.id = "CursorLayer";
-    // canvas.width = 500;
-    // canvas.height = 600;
-    // canvas.style.zIndex = 8;
-    // canvas.style.position = "absolute";
-    // canvas.style.border = "1px solid";
-    // canvas.style.backgroundColor = "grey";
-    //
-    //
-    // var body = document.getElementsByTagName("body")[0];
-    // body.appendChild(canvas);
-    //
-    // cursorLayer = document.getElementById("CursorLayer");
-    //
-    // console.log(cursorLayer);
-
-    var c_canvas = document.getElementById("canvas");
-
-    var context = c_canvas.getContext("2d");
-
-        c_canvas.width = 90;
-        c_canvas.height = 90;
-
-        for (var x = 0.5; x < 90; x += 30) {
-            context.moveTo(x, 0);
-            context.lineTo(x, 90);
-        }
-
-        for (var y = 0.5; y < 90; y += 30) {
-            context.moveTo(0, y);
-            context.lineTo(90, y);
-        }
-
-    context.strokeStyle = "#888";
-    context.stroke();
-});
-
-var example = document.getElementById("example"),
-    ctx     = example.getContext('2d');
-example.width  = 640;
-example.height = 480;
-ctx.strokeRect(15, 15, 266, 266);
-ctx.strokeRect(18, 18, 260, 260);
-ctx.fillRect(20, 20, 256, 256);
-for ( var i = 0; i < 8; i += 2) {
-    for (var j = 0; j < 8; j += 2) {
-        ctx.clearRect(20 + i * 32, 20 + j * 32, 32, 32);
-        ctx.clearRect(20 + (i + 1) * 32, 20 + (j + 1) * 32, 32, 32);
-    }
-}
-
-
-
-// function draw() {
-//     var canvas = document.getElementById('canvas');
-//     if (canvas.getContext) {
-//         var ctx = canvas.getContext('2d');
-//
-//         ctx.fillRect(25,25,100,100);
-//         ctx.clearRect(45,45,60,60);
-//         ctx.strokeRect(50,50,50,50);
-//     }
-// }
